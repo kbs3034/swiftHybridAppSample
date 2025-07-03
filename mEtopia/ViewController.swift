b2a7bb8520ce99a905b4091b93bb2d35bee5ee82
banseog <kbs3034@naver.com>
2025-07-03 09:11:20 +0900

    Initial Commit

---
+++ mEtopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mEtopia
+//
+//  Created by sbk on 7/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mEtopiaTests/mEtopiaTests.swift
@@ -0,0 +1,17 @@
+//
+//  mEtopiaTests.swift
+//  mEtopiaTests
+//
+//  Created by sbk on 7/3/25.
+//
+
+import Testing
+@testable import mEtopia
+
+struct mEtopiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
